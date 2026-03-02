d35addc15656980bce85552bcfe70ba271f51f27
dixitram <dixitram2005@gmail.com>
2026-03-02 21:24:16 +0530

    added config

---
+++ config.js
@@ -0,0 +1,9 @@
+// ============================================
+//  CASCA LITE — Configuration
+// ============================================
+
+const ENV = {
+  CLOUD_STORAGE_BIN_ID: 'b4bfb62132c6',
+  CLOUD_STORAGE_API_KEY: 'justThePublicKey',
+  CLOUD_STORAGE_BASE_URL: 'https://json.extendsclass.com/bin/',
+};
